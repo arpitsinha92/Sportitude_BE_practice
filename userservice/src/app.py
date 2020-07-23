fbfdcb80d11f5f604fdccd91c2c483971b79211b
swetha <swetha.s@pharmeasy.in>
2020-07-23 19:22:12 +0530

    Basic API Created

---
+++ userservice/src/app.py
@@ -0,0 +1,10 @@
+from flask import Flask, request, jsonify
+from markupsafe import escape
+
+app = Flask(__name__)
+
+@app.route('/api/<username>', methods=['GET'])
+def hello(username):
+    return jsonify({ "name":escape(username), "age":28, "city":"New York"})
+
+app.run()
